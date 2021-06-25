ea8990bccc0cfcdd2d40d0c0bfd874c1f257e9e4
Sangosanya Segun <sangosanyasegun@yahoo.com>
2021-06-25 04:30:45 +0100

    review2

--- src/blockchain.js
+++ src/blockchain.js
@@ -99,7 +99,7 @@ class Blockchain {
      */
     requestMessageOwnershipVerification(address) {
         return new Promise((resolve) => {
-            resolve(`$address:${new Date().getTime().toString().slice(0,-3)}:starRegistry`);
+            resolve(`${address}:${new Date().getTime().toString().slice(0,-3)}:starRegistry`);
         });
     }
 
